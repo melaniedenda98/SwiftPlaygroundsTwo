6544461362c5ea151c8e832b40340520be8779a7
melaniedenda98 <mdenda@hawaii.edu>
2022-09-22 03:17:17 -0700

    Completed Parts 1-4

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
 var greeting = "Hello, playground"
+// part 2
+var goodGrade = "I've been doing all my work"
+
+//line 5 is a conditional statement
+if goodGrade == "I'm not doing very well" {
+    // then the code block will execute
+} else
+{
+    // part 3
+    print("I need to work harder in this class")
+}
+//part 4
+var gradeA = true
+
+if gradeA == true
+{
+    print("I really like this class")
+} else {
+    print("I'm not sure about coding in Swift")
+}
